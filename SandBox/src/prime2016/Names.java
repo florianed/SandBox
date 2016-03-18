4deac473ffc863fb4754db0f5103a945f10e4f4b
Floriane Dahman <floriane.dahman@hotmail.fr>
2016-03-18 16:28:50 +0100

    Floriane

---
+++ SandBox/src/prime2016/Names.java
@@ -0,0 +1,12 @@
+package prime2016;
+
+public class Names {
+	
+	public static void names() {
+		System.out.println("Floriane Dahman");
+	}
+	
+	public static void main(String[] args) {
+		names();
+	}
+}
